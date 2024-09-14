a2150a5c73b7cc45eb4dcf618fcbed5446f72d11
Juan Antonio <1317580+JuantonLab@users.noreply.github.com>
2024-09-14 00:23:04 -0500

    feat: change the default metadata values and add custom

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Pricing Page",
+  description: "Pricing description",
+  keywords: ["pricing", "cars"]
+}
+
 export default function PricingPage(){
   return(
     <>

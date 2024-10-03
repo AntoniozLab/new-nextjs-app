12f74bd4e5d4d3d0a3fb4ffaa0573276fc498f16
Juan Antonio <1317580+JuantonLab@users.noreply.github.com>
2024-10-03 00:06:37 -0500

    feat: create main route dir

---
+++ app/(routes)/layout.tsx
@@ -0,0 +1,13 @@
+
+export default function GeneralLayout({
+ children
+}: {
+ children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <h1>General Layout</h1>
+      {children}
+    </div>
+  );
+}

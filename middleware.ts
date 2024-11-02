f8a0ad572998cd0d63f071cb94c30e25513fa99a
Juan Antonio <1317580+JuantonLab@users.noreply.github.com>
2024-11-01 23:13:11 -0500

    feat: add first middleware

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const protectedPaths = ['/home', '/departments', '/categories']
+export default function ProtectedMiddleware (request: NextRequest) {
+  console.log("protected route");
+
+  const { nextUrl } = request;
+
+  if (protectedPaths.some((path) => nextUrl.pathname.startsWith(path))) {
+    const token = nextUrl.searchParams.get('token');
+    if (!token ){
+      const loginUrl = new URL('/login', request.url);
+      return NextResponse.redirect(loginUrl)
+    }
+  }
+  return NextResponse.next();
+}

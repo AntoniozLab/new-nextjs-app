f4537fbe09cd9e36d8bb7a2ef2b4199ebcfd48e8
Juan Antonio <1317580+JuantonLab@users.noreply.github.com>
2024-12-09 16:30:34 -0500

    feat: replace a by Link next component

--- components/BottonNav/BottonNav.tsx
+++ components/BottonNav/BottonNav.tsx
@@ -1,10 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 export const BottonNav = () => {
   return (
     <div className="flex bg-primary bg-opacity-30 py-1 px-2">
-      <a className="mr-2" href="/?token=x">Home</a>
-      <a className="mr-2" href="/categories?token=x">Categories</a>
+
+      <Link className="mr-2" href="/home?token=x">Home</Link>
+      <Link className="mr-2" href="/categories?token=x">Categories</Link>
       <a className="mr-2" href="/departments?token=x">Departments</a>
     </div>
   )

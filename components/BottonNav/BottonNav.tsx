d5a871785cc9f399a0eb5ba09c4d629e065996f0
Juan Antonio <1317580+JuantonLab@users.noreply.github.com>
2024-12-09 17:14:06 -0500

    feat: create navigation list

--- components/BottonNav/BottonNav.tsx
+++ components/BottonNav/BottonNav.tsx
@@ -1,13 +1,29 @@
 import Link from 'next/link'
 import React from 'react'
 
+const navItems = [
+  {
+    path: '/home?token=x',
+    label: 'Home'
+  },
+  {
+    path: '/categories?token=x',
+    label: 'Categories'
+  },
+  {
+    path: '/departments?token=x',
+    label: 'Departments'
+  }
+];
+
 export const BottonNav = () => {
   return (
     <div className="flex bg-primary bg-opacity-30 py-1 px-2">
-
-      <Link className="mr-2" href="/home?token=x">Home</Link>
-      <Link className="mr-2" href="/categories?token=x">Categories</Link>
-      <a className="mr-2" href="/departments?token=x">Departments</a>
+      {
+        navItems.map(({ path, label})=> (
+          <Link className="mr-10" href={path} key={path}>{label}</Link>
+        ))
+      }
     </div>
   )
 }

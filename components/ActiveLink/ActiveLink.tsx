bc061c83224d6bfda0108711fdd30d8fad79439f
Juan Antonio <1317580+JuantonLab@users.noreply.github.com>
2024-12-09 21:26:10 -0500

    feat: create active link component

---
+++ components/ActiveLink/ActiveLink.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Link from 'next/link';
+import React from 'react';
+import style from './ActiveLink.module.css';
+import { usePathname } from 'next/navigation';
+
+
+interface IProps {
+  path: string;
+  text: string;
+}
+
+export const ActiveLink= ({ path, text }: IProps) => {
+  const pathName = usePathname();
+  return (
+    <Link
+      className={`${style.link} ${pathName === path && style['active-link']}`}
+      href={path}
+      key={path}
+    >
+      {text}
+    </Link>
+  );
+};
